ce54d8647ab7d9132fe9a1ef0bd42bdce26b0345
Doni Setiawan <donisetiawan.050@gmail.com>
2019-12-26 19:05:27 +0700

    Understanding the order of Connect middleware

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ const connect = require('connect');
 
 const app = connect();
 
+const logger = (req, res, next) => {
+  console.log(req.method, req.url);
+  next();
+};
+
 const helloWorld = (req, res) => {
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World');
 };
 
+app.use(logger);
 app.use(helloWorld);
 
 app.listen(3000);
