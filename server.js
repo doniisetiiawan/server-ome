c469d116b1f8f64772648d49d6fe98a2bef5c6cb
Doni Setiawan <donisetiawan.050@gmail.com>
2019-12-26 18:19:22 +0700

    Developing Node.js web applications

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
-const fs = require('fs');
+const http = require('http');
 
-fs.readFile('/etc/hosts', 'utf8', (err, data) => {
-  if (err) {
-    return console.log(err);
-  }
-  console.log(data);
-});
+http
+  .createServer((req, res) => {
+    res.writeHead(200, {
+      'Content-Type': 'text/plain',
+    });
+
+    res.end('Hello World');
+  })
+  .listen(3000);
+
+console.log('Server running at http://localhost:3000/');
