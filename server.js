e8c6951183d1e06773ac5a8a54d401962504fb70
Doni Setiawan <donisetiawan.050@gmail.com>
2019-12-26 18:26:26 +0700

    Connect middleware

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const connect = require('connect');
 
 const app = connect();
+
+const helloWorld = (req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+};
+
+app.use(helloWorld);
+
 app.listen(3000);
 console.log('Server running at http://localhost:3000/');
