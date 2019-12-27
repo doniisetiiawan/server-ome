ebd084d2474216ed1d28a5ea5511eefbc4a8fe09
Doni Setiawan <donisetiawan.050@gmail.com>
2019-12-27 19:18:12 +0700

    Extending your Mongoose schema

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -5,9 +5,60 @@ const { Schema } = mongoose;
 const UserSchema = new Schema({
   firstName: String,
   lastName: String,
-  email: String,
-  username: String,
+  email: {
+    type: String,
+    index: true,
+  },
+  username: {
+    type: String,
+    trim: true,
+    unique: true,
+  },
   password: String,
+  website: {
+    type: String,
+    set(url) {
+      if (!url) {
+        return url;
+      }
+      if (
+        url.indexOf('http://') !== 0
+        && url.indexOf('https://') !== 0
+      ) {
+        url = `http://${url}`;
+      }
+
+      return url;
+    },
+    get(url) {
+      if (!url) {
+        return url;
+      }
+      if (
+        url.indexOf('http://') !== 0
+        && url.indexOf('https://') !== 0
+      ) {
+        url = `http://${url}`;
+      }
+      return url;
+    },
+  },
+  created: {
+    type: Date,
+    default: Date.now,
+  },
 });
+
+UserSchema.virtual('fullName')
+  .get(function () {
+    return `${this.firstName} ${this.lastName}`;
+  })
+  .set(function (fullName) {
+    const splitName = fullName.split(' ');
+    this.firstName = splitName[0] || '';
+    this.lastName = splitName[1] || '';
+  });
+
+UserSchema.set('toJSON', { getters: true, virtuals: true });
 
 mongoose.model('User', UserSchema);
