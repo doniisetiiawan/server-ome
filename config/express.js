6c130351a079d60deece8d1498a2ea5d93d15f37
Doni Setiawan <donisetiawan.050@gmail.com>
2020-01-04 18:59:30 +0700

    Installing Socket.io

--- config/express.js
+++ config/express.js
@@ -1,16 +1,23 @@
 /* eslint-disable global-require */
+const http = require('http');
+const socketio = require('socket.io');
 const express = require('express');
 const morgan = require('morgan');
 const compress = require('compression');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 const flash = require('connect-flash');
 const passport = require('passport');
 const config = require('./config');
+const configureSocket = require('./socketio');
 
-module.exports = () => {
+module.exports = (db) => {
   const app = express();
+
+  const server = http.createServer(app);
+  const io = socketio.listen(server);
 
   if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
@@ -27,11 +34,16 @@ module.exports = () => {
   app.use(bodyParser.json());
   app.use(methodOverride());
 
+  const mongoStore = new MongoStore({
+    mongooseConnection: db.connection,
+  });
+
   app.use(
     session({
       saveUninitialized: true,
       resave: true,
       secret: config.sessionSecret,
+      store: mongoStore,
     }),
   );
 
@@ -45,6 +57,8 @@ module.exports = () => {
   require('../app/routes/articles.server.routes.js')(app);
 
   app.use(express.static('./public'));
+
+  configureSocket(server, io, mongoStore);
 
   return app;
 };
--- server.js
+++ server.js
@@ -2,12 +2,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 const configureMongoose = require('./config/mongoose');
 const configureExpress = require('./config/express');
-
 const configurePassport = require('./config/passport');
 
-configureMongoose();
+const db = configureMongoose();
 
-const app = configureExpress();
+const app = configureExpress(db);
 configurePassport();
 app.listen(3000);
 module.exports = app;
